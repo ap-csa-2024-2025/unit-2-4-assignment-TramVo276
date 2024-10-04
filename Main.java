79f61b0bbc89f498120505df8df40478d9f5eb6f
TramVo276 <tvo941352@gmail.com>
2024-10-04 10:16:38 -0700

    asdg

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(cream, butter, 3);
+    }
+
+    public static String LastFirstN(String s1, String s2, int)
+    {
+        String outPut = new String();
+        String  LastW = s1.substring(s1.length() - n);
+        String FristW = s2.substring(0, n);
+        ouPut = LastW + FristW;
+        return outPut;
+
+
+
+    }
+
+    
+    
+}
